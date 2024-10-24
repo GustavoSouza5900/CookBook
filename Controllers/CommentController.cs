ec467ed10548777eff1db8e1e2e79b407d60826d
Gustavo Henrique da Silva Souza <132629467+GustavoSouza5900@users.noreply.github.com>
2024-10-24 11:33:39 -0300

    mudanças no comentário
    adicionado id do usuário e receitas ao ao comentário no banco

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -13,13 +13,19 @@ public class CommentController : Controller {
     }
 
     [HttpGet]
-    public ActionResult Save() {
-        return View();
+    public ActionResult Save(int RecipeId) {
+        if (ViewBag.Auth = true) {
+            return View(RecipeId);
+        }
+        
+        //else
+        return RedirectToAction("Login", "User");
     }
 
 
     [HttpPost]
     public void Save(Comment model) {
+        model.UserId = HttpContext.Session.GetInt32("UserId");
         db.Comments.Add(model);
         db.SaveChanges();
     }
